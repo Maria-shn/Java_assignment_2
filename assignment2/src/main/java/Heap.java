9e1eb5394b6f77f60133e80641a567e5c264d50d
Maria <maria.shn.es@gmail.com>
2023-04-07 13:47:07 +0300

    implementind add method in Heap class

--- assignment2/src/main/java/Heap.java
+++ assignment2/src/main/java/Heap.java
@@ -7,26 +7,65 @@ public class Heap<T extends Comparable<T>>  {
 
     public Heap(){
         this.heap = (T[]) Array.newInstance(Comparable.class, firstSize);
-        this.max = 0;
         this.size = 1;
+        this.max = 0;
     }
 
     public Heap(int n){
         this.heap = (T[]) Array.newInstance(Comparable.class, n);
-        this.max = 0;
-        this.size = n;    
+        this.size = n;
+        this.max = 0;  
     }
 
     public void add(T t) {
         if(this.max != this.size){
-            int index = max+1;
-            heap[index] = t;
-            while(heap[index].compareTo(heap[index/2])>0){
-                
-
+            percUp(max+1, t, this.heap);
+            this.max +=1;
             }
+        else{
+            Heap<T> bigHeap = new Heap(size*2);
+            int i =0;
+            
+        }
 
         }
+    
+    /**
+     * adds t to its proper position in the array
+     * @param index
+     * @param t
+     * @param arr
+     */
+    public void percUp(int index, T t, T[] arr){
+        int parInd = (int) (index/2);
+        if(index ==1){
+            arr[index] = t;
+        }
+        else if(t.compareTo(arr[parInd])<0){
+            arr[index] = t;
+        }
+        else{
+            arr[index] = arr[parInd];
+            percUp(parInd, t, arr);
+        }
+
     }
+
+    /**
+     * when there is no space left in the current array percdown will copy the current array to the emptyArr 
+     * and add t to the right position
+     * TO BE FINISHED
+     */
+
+     public void percDown(int index, T t, T[] arr, T[] emptyArr){
+        if(t.compareTo(arr[index])>0){
+            emptyArr[index] = t;
+
+        }
+     }
+
+
+
+    
 
 }
